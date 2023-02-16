01460f91148d844b81edbe5014303912b1b09b98
Lawal Rukayat Abimbola <49238805+Rukkylaw@users.noreply.github.com>
2023-02-17 00:32:37 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 include <stdion.h>
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+puts("Programming is like building a multilingual puzzle");
+return(0);
 }
