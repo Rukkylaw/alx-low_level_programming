449a0aa20dc407fadeb9453f936778344d092f9f
Rukkylaw <Bimbola1995@gmail.com>
2023-02-16 15:42:09 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include <stdio.h>
-=======
-include <stdion.h>
->>>>>>> 01460f91148d844b81edbe5014303912b1b09b98
-int main (void)
+int main()
 {
 puts("Programming is like building a multilingual puzzle");
 return(0);
