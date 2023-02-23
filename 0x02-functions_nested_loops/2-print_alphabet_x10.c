25618e136d346e1bc30591416fd7d10af78fca18
Lawal Rukayat Abimbola <49238805+Rukkylaw@users.noreply.github.com>
2023-02-23 14:06:13 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,26 +1,20 @@
 #include "main.h"
-
 /**
  * main - check the code
- *
+ * print_alphabet_x10 -  prints 10 times the alphabet in lowercase followed by a new line
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 char k = 'a';
 int i;
-for(i=0; i<10; i++)
+for(i = 0; i < 10; i++)
 {
- while (k <= 'z')
- {
- _putchar(k);
- k++;
- }
- _putchar('\n');
-}
-}
-int main()
+while (k <= 'z')
 {
-print_alphabet_x10();
-return (0);
+_putchar(k);
+k++;
+}
+_putchar('\n');
+}
 }
