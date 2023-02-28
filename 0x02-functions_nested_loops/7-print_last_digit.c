265a85525dd1d0d9216addc3eff8cb9a7663b2cc
Lawal Rukayat Abimbola <49238805+Rukkylaw@users.noreply.github.com>
2023-02-28 13:25:22 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * 7-print_last_digit-prints the last digit of an interger.
- * using main.h header file
+ * print_last_digit-prints the last digit of an interger using main.h header file
+ * @a - represents the int
  * Return:last digit.
  */
 int print_last_digit(int a)
