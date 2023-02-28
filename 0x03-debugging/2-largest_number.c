ab381f57d3b21df2db7d66420b3311ebb64884b0
Lawal Rukayat Abimbola <49238805+Rukkylaw@users.noreply.github.com>
2023-02-28 11:09:18 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,24 +11,19 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
 {
 largest = b;
 }
-else if (a > c)
-{
-largest = a;
-}
-else if (b > a && b > c)
-{
-largest = b;
-}  
-else if (c > b && b > a)
+else
 {
 largest = c;
 }
