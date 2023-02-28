b8d82ee9e89516268e68a71536e702c88b51fe6f
Lawal Rukayat Abimbola <49238805+Rukkylaw@users.noreply.github.com>
2023-02-28 20:33:44 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts2 -  prints every other character of a
- * string, starting with the first 
+ * string, starting with the first
  * character, followed by a new line.
  * @str: string to use
  */
@@ -15,4 +15,3 @@ i += 2;
 }
 putchar('\n');
 }
-
